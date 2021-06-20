d313ba7409c409e5b511633d3832d4571815bc98
NABA7082 <69723668+NABA7082@users.noreply.github.com>
2021-06-20 19:24:53 +0530

    Update tha7.js

--- tha7/tha7.js
+++ tha7/tha7.js
@@ -15,3 +15,30 @@ console.log(student);
 delete student.rollno;
 console.log(student);
 
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
+var size=Object.keys(student).length;
+console.log(size);
+
+var lib=
+[ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true },
+ { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, 
+ { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games',
+  readingStatus: false }]; 
+  for(let i=0;i<lib.length;i++)
+  {
+    var book=" "+lib[i].title+" "+'by'+" "+lib[i].author+"."
+    if(lib[i].readingStatus===true)
+    {
+      console.log("Already read"+book);
+    }
+    else{
+      console.log("You still need to read"+book);
+    }
+  }
+  
+
+
+
+
+
+
